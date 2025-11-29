ccbb5d36b4115928cd59d08695c88f43f1ce0ed5
Md-Yasin <mohamed786783@gmail.com>
2025-11-29 12:15:13 +0530

    Fix CORS for all origins

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from fastapi.middleware.cors import CORSMiddleware
 import httpx
 import socket
 from urllib.parse import urlparse
